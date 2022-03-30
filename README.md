1f106482302e3daa643aa69f7b1de89fd8332e8c
Leo Lamprecht <leo.lamprecht@icloud.com>
2022-03-30 15:11:55 +0200

    Added basics

---
+++ README.md
@@ -0,0 +1,3 @@
+# tailwindcss-conditions
+
+NEW
---
+++ index.ts
@@ -0,0 +1,31 @@
+class TailwindCondition {
+  // Using `any` here will make sure the type is inherited
+  // from the actual argument that is passed, since the argument
+  // might either be a string or a number.
+  value: any;
+
+  // Allows for passing a custom suffix that will be compared
+  // with the Tailwind utility that is passed via `util()`.
+  suffix?: string;
+
+  constructor(value: number | string, suffix?: string) {
+    this.value = value;
+    this.suffix = suffix;
+  }
+
+  util(names, conditions?) {
+    const utils = names.split(' ');
+    const match = this.suffix || `-${this.value}`;
+
+    const validName = utils.find((util) => util.endsWith(match));
+    const conditionsMet = typeof conditions === 'undefined' ? true : conditions;
+
+    if (!validName) {
+      throw new Error(`Tailwind utilities "${names}" don't include "${match}" match.`);
+    }
+
+    return conditionsMet ? names : null;
+  }
+}
+
+export default TailwindCondition;
